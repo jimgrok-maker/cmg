f85f751a95e19c60a97056a1e486b99d42c42633
jimgrok-maker <jimdomandroid@gmail.com>
2026-09-25 15:25:30 -0400

    Use compileSdk 34 and add activity dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.cmg.erie"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.cmg.erie"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "0.1.0"
     }
@@ -31,5 +31,6 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.activity:activity:1.9.3")
     implementation("androidx.webkit:webkit:1.12.1")
 }
